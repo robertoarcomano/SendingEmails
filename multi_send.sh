eb526ed8c65690f3ef4a4cc7f2136862108d5c2c
Roberto Arcomano <info@robertoarcomano.com>
2024-01-14 20:21:34 +1300

    Added pause

--- multi_send.sh
+++ multi_send.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LIST_RECIPIENT="/home/berto/Documents/NewZealand/Jobs/final_list.txt"
-LIST_RECIPIENT="/home/berto/Documents/Help/postfix/list2.txt"
+LIST_RECIPIENT="/home/berto/Documents/Help/SendingEmails/list2.txt"
 EOI="/home/berto/Documents/CV/Expression_of_interest.txt"
 CV="/home/berto/Documents/CV/CV_Arcomano_Roberto.pdf"
 HEADER="From: Roberto Arcomano <info@robertoarcomano.com>"
@@ -11,4 +11,5 @@ cat "$LIST_RECIPIENT"|while read EMAIL; do
     echo $i $EMAIL
     let i=i+1
     cat "$EOI" | mail -A "$CV" -a "$HEADER" -s "$SUBJECT" "$EMAIL"
+    read -r b</dev/tty
 done
